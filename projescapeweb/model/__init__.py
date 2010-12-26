4897e1744f839d07b1dc0b5a232e2b0342521fc3
Sun Ning <classicning@gmail.com>
2010-12-26 16:17:39 +0800

    user models

--- projescapeweb/model/__init__.py
+++ projescapeweb/model/__init__.py
@@ -1,6 +1,8 @@
 """The application's model objects"""
 from projescapeweb.model.meta import Session, Base
 
+from projescapeweb.model.user import User, UserDetail
+
 
 def init_model(engine):
     """Call me before using any of the tables or classes in the model"""
---
+++ projescapeweb/model/user.py
@@ -0,0 +1,42 @@
+from sqlalchemy import Column, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.types import Integer, String, DateTime, PickleType
+
+from projescapeweb.model.meta import Base
+
+class User(Base):
+    """the user model, for user authentication"""
+
+    __tablename__ = 'user'
+
+    _id = Column(Integer, primary_key=True)
+    username = Column(String(32))
+    password = Column(String(64))
+    email = Column(String(32))
+    email_md5 = Column(String(64))
+    open_id = Column(String(128))
+    open_id_alt = Column(String(128))
+    created_time = Column(DateTime())
+    user_detail_id = Column(Integer, ForeignKey("user_detail._id"))
+    user_detail = relationship("UserDetail")
+
+    def __init__(self):
+        pass
+        
+class UserDetail(Base):
+    """the user detail model, detail information"""
+
+    __tablename__ = 'user_detail'
+
+    _id = Column(Integer, primary_key=True)
+    last_login = Column(DateTime())
+    display_name = Column(String(length=32, convert_unicode=True, assert_unicode=True))
+    job_title = Column(String(length=128, convert_unicode=True, assert_unicode=True))
+    description = Column(String(length=256, convert_unicode=True, assert_unicode=True))
+    location = Column(String(length=64, convert_unicode=True, assert_unicode=True))
+    web_site = Column(String(length=64))
+    social_links = Column(PickleType())
+
+    def __init__(self):
+        pass 
+
