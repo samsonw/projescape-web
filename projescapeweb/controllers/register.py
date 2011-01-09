5905d1853761943b108080202004b77f37ec1368
Sun Ning <classicning@gmail.com>
2011-01-09 23:20:34 +0800

    add recaptcha

--- projescapeweb/controllers/register.py
+++ projescapeweb/controllers/register.py
@@ -2,9 +2,13 @@ import logging
 import uuid
 from datetime import datetime
 
-from pylons import request, response, session, tmpl_context as c, url, app_globals as g
+from pylons import request, response, session, tmpl_context as c, url, app_globals as g, config
+from pylons.i18n import _
 from pylons.controllers.util import abort, redirect
 from pylons.decorators.rest import restrict
+
+from sqlalchemy import or_
+from recaptcha.client import captcha
 
 from projescapeweb.lib.base import BaseController, render, Session
 from projescapeweb.lib.helpers import md5, sendmail
@@ -15,6 +19,7 @@ log = logging.getLogger(__name__)
 class RegisterController(BaseController):
 
     def index(self):
+        c.pubkey = config['recaptcha.public']
         return render('register/index.html')
 
     def pending(self):
@@ -37,19 +42,29 @@ class RegisterController(BaseController):
     
     @restrict('POST')
     def save(self):
+        ## check captcha
+        r = captcha.submit(request.params['recaptcha_challenge_field'],
+                request.params['recaptcha_response_field'],
+                config['recaptcha.private'],
+                request.environ['REMOTE_ADDR'])
+        if not r.is_valid:
+            session['flash'] = _('Invalid captcha')
+            redirect(url(controller='register', action='index'))
+            return;
+
         username = request.params['username']
         password = request.params['passwd']
         password_2 = request.params['passwd_2']
         email = request.params['email']
 
         if password != password_2:
-            session['flash'] = 'Password missmatch' 
+            session['flash'] = _('Password missmatch')
             redirect(url(controller='register', action='index'))
             return;
 
         user_q = Session.query(User)
-        if user_q.filter_by(username=username).first() is not None:
-            session['flash'] = 'Username obtained by others'
+        if user_q.filter(or_(User.username==username, User.email==email)).first() is not None:
+            session['flash'] = _('Username/Email obtained by others')
             redirect(url(controller='register', action='index'))
             return;
 
@@ -74,7 +89,7 @@ class RegisterController(BaseController):
                 action='activate', id=activation_id)
         c.activation_link = activation_link
         content = render('register/activation.email')
-        sendmail('no-reply@projescape', email, 'Activation your account in Projescape', content)
+        sendmail('no-reply@projescape', email, _('Activation your account in Projescape'), content)
 
         ## redirect user to information page
         session['user.email'] = email
--- setup.py
+++ setup.py
@@ -15,6 +15,8 @@ setup(
     install_requires=[
         "Pylons>=1.0",
         "SQLAlchemy>=0.6",
+        "Redis>=2.2.0",
+        "recaptcha-client>=1.0.5",
     ],
     setup_requires=["PasteScript>=1.6.3"],
     packages=find_packages(exclude=['ez_setup']),
